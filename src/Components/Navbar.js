a5eed5d8485869266848ca7707c244e711b81054
surendra019 <iamsurendra019@gmail.com>
2024-09-11 23:13:49 +0530

    f

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar() {
                     <a className="navbar-brand" href="#">World Today</a>
                     <div>|&nbsp;&nbsp;</div>
                     <div>{`${time} ${day}`}</div>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -33,7 +33,7 @@ export default function Navbar() {
 
                         </ul>
                      
-                    </div>
+                    </div> */}
                 </div>
             </nav>
             <div className={global_styles.navbar_options_container}>
